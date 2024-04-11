d29587147e9561775f00cfae364062cf929a2408
Your NameKaminariSora <nonthacha.h@gmail.com>
2024-04-11 20:40:26 +0700

    update about page

--- cpe-web-application/src/1st_Components/about.js
+++ cpe-web-application/src/1st_Components/about.js
@@ -1,13 +1,30 @@
-import "./about.css"
-import Header from "./header"
+import { text } from "@fortawesome/fontawesome-svg-core";
+import "./about.css";
+import { useRef, useEffect } from "react";
 
-const about = () => {
+const About = () => {
+    const textRef = useRef(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (textRef.current) {
+                const value = window.scrollY;
+                textRef.current.style.marginRight = value * 0.75 + 'px';
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <div>
-            <Header/>
-            <div>Hello</div>
-        </div>
-    )
-}
+        <div className="About-container">
+            <h1 ref={textRef}>Hello</h1>
+        </div> 
+    );
+};
 
-export default about
+export default About;
