ea49b30047029003c870b62dd567d0f1ba98dc77
Your NameKaminariSora <nonthacha.h@gmail.com>
2024-05-05 15:37:03 +0700

    Revert "Merge branch 'Demo' into baitarn"
    This reverts commit 0dfd5667efb436254333f442ef39e22bc07cff47, reversing
    changes made to 6ef3f1d4c014836e0428f73433b1ccdf1066d192.

--- cpe-web-application/src/App.js
+++ cpe-web-application/src/App.js
@@ -5,21 +5,8 @@ import PerProfile from './1st_Components/PersonelPage/PerProfile/PerProfile.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Firstpage/>
     </div>
   );
 }
